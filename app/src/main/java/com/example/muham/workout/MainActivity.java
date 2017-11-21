26b6d3982f3eb167e69716c00a5f4c5b2298ea9e
DESKTOP-SOGRI79\muham <zabidin@gmu.edu>
2017-11-21 14:31:19 -0500

    test2

--- app/src/main/java/com/example/muham/workout/MainActivity.java
+++ app/src/main/java/com/example/muham/workout/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.muham.workout;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -16,6 +18,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
 
         Button start = (Button) findViewById(R.id.Start);
         Button addworkout = (Button) findViewById(R.id.add);
+        TextView yLabel = (TextView)findViewById(R.id.yLabel);
+        TextView mLabel = (TextView)findViewById(R.id.mLabel);
+        TextView dLabel = (TextView)findViewById(R.id.dLabel);
+        TextView eLabel = (TextView)findViewById(R.id.eLabel);
+
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MMMM/d/E", Locale.US); // Set your locale!
+        String strDate = sdf.format(cal.getTime());
+
+        String[] values = strDate.split("/",0);
+
+
+        yLabel.setText(values[0]);
+        mLabel.setText(values[1]);
+        dLabel.setText(values[2]);
+        eLabel.setText(values[3]);
 
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -37,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
                 setContentView(R.layout.add);
             }
         });
+
+
 
 
     }
